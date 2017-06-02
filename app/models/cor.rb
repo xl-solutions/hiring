cebe74a84c22cfac1a1ffb749c54728766238fa8
Roger <rogerdelnobre@gmail.com>
2017-06-02 00:48:02 -0300

    #5 Criando relacionamento das entidades nos models
    Signed-off-by: Roger <rogerdelnobre@gmail.com>
    
    Signed-off-by: Roger <rogerdelnobre@gmail.com>

--- app/models/cor.rb
+++ app/models/cor.rb
@@ -1,2 +1,6 @@
 class Cor < ApplicationRecord
+	has_many :produtos
+
+	validates :nome, presence:{
+			  message: 'Nome da Cor não pode ser vazio!'}
 end
--- app/models/modelo.rb
+++ app/models/modelo.rb
@@ -1,2 +1,6 @@
 class Modelo < ApplicationRecord
+	has_many :produtos
+
+	validates :nome, presence: {
+			  message: 'Nome do Modelo não pode ser vazio!'}
 end
