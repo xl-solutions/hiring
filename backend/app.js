9252fe6d4654f6887ec933c9285b36ae0195a07f
fecabianchi <felipecabianchi@gmail.com>
2017-06-29 02:46:51 -0300

    cors removed path added

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import compression from 'compression'
+import path from 'path'
 import stockRoute from './routes/stocks'
 
 const app = express()
@@ -10,6 +11,7 @@ app.use(compression())
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(`${__dirname}/../frontend/dist`)))
 
 app.use('/stocks', stockRoute)
 
