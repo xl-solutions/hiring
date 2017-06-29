0279fe24f9b4dea491a864041c55a5f5d7f02af6
fecabianchi <felipecabianchi@gmail.com>
2017-06-29 14:11:46 -0300

    stocks routes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import compression from 'compression'
+import stockRoute from './routes/stocks'
 
 const app = express()
 
@@ -9,5 +10,7 @@ app.use(compression())
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/stocks', stockRoute)
 
 export default app
