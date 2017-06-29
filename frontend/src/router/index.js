ffc1835d44772ac5264b54b895de3e87fc624dd3
fecabianchi <felipecabianchi@gmail.com>
2017-06-29 14:37:38 -0300

    routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Portifolio from '@/components/Portifolio'
+import NewStock from '@/components/NewStock'
+import History from '@/components/History'
+import Gains from '@/components/Gains'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    { path: '/', name: 'dashboard', component: Portifolio, meta: { title: 'Portfolio' }
+    },
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: '/stock', component: NewStock, name: 'stock', meta: { title: 'New Stock' }
+    },
+    {
+      path: '/history', component: History, name: 'history', meta: { title: 'History' }
+    },
+    {
+      path: '/gains', component: Gains, name: 'gains', meta: { title: 'Gains' }
     }
   ]
 })
