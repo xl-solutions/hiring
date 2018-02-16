cef6a6e594aa7223b261178b9a688c168b2acbff
EvertonMirand <everton.mirandav@gmail.com>
2018-02-16 20:26:55 -0200

    Primeiro Commit

---
+++ Mobile_APP/XL_APP/XL_APP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XL_APP
+//
+//  Created by Everton Miranda Vitório on 16/02/18.
+//  Copyright © 2018 Everton Miranda Vitório. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
