988b1532184814f6875061abe511bc6737095406
Marco Aurelio Maturana Silveiro <marco.maturana.ro@gmail.com>
2018-02-16 22:37:31 -0400

    Test created for route /stocks/:stock_name/history

--- server/controllers/stocks_controller.js
+++ server/controllers/stocks_controller.js
@@ -1,9 +1,21 @@
 module.exports.quote = (app, req, res) => {
-  res.json({ "name": "PETR4.SA", "lastPrice": 25.11, "pricedAt": "2017-06-23T14:15:16Z" })
+  res.json({ name: "PETR4.SA", lastPrice: 25.11, pricedAt: "2017-06-23T14:15:16Z" })
 }
 
 module.exports.history = (app, req, res) => {
-
+  res.json(
+    { name: "PETR4.SA",
+      prices: [
+        { opening: 14.67,
+          low: 14.57,
+          high: 14.89,
+          closing: 14.85,
+          pricedAt: '2017-04-04' },
+        { opening: 15.05,
+          low: 14.50,
+          high: 15.16,
+          closing: 14.57,
+          pricedAt: '2017-04-05' }]})
 }
 
 module.exports.compare = (app, req, res) => {
--- server/tests/controllers/stocks_controller_test.js
+++ server/tests/controllers/stocks_controller_test.js
@@ -6,7 +6,11 @@ let
 
 chai.use(chaiHttp)
 
-const stock_name = 'PETR4.SA'
+const
+  stock_name = 'PETR4.SA',
+  historyFrom = '2018-01-01',
+  historyTo = '2018-01-02'
+
 
 let expect = chai.expect
 
@@ -24,8 +28,8 @@ describe('Test in stocks_controller', () => {
       .get('/stocks/' + stock_name + '/quote')
       .end((err, res) => {
         expect(res.status).to.equal(200)
-        expect(res.body).to.be.an('object');
-        expect(res.body).have.all.keys('name', 'lastPrice', 'pricedAt');
+        expect(res.body).to.be.an('object')
+        expect(res.body).have.all.keys('name', 'lastPrice', 'pricedAt')
         expect(res.body.name).to.be.a('string')
         expect(res.body.lastPrice).to.be.a('number')
         expect(res.body.pricedAt).to.be.a('string')
@@ -33,7 +37,32 @@ describe('Test in stocks_controller', () => {
         //check data format is valid
         expect(isDateISO8601(res.body.pricedAt)).to.equal(true)
 
-        done();
-      });
-  });
-});
+        done()
+      })
+  })
+
+  it('Test route /stocks/:stock_name/history', (done) => {
+    chai.request(app)
+      .get('/stocks/' + stock_name + '/history?from=<' + historyFrom + '>&to=<' + historyTo + '>')
+      .end((err, res) => {
+        expect(res.status).to.equal(200)
+        expect(res.body).to.be.an('object')
+        expect(res.body).have.all.keys('name', 'prices')
+        expect(res.body.name).to.be.a('string')
+        expect(res.body.prices).to.be.an('array')
+
+        res.body.prices.forEach((price) => {
+          expect(price).have.all.keys('opening', 'low', 'high', 'closing' ,'pricedAt')
+          expect(price.opening).to.be.a('number')
+          expect(price.low).to.be.a('number')
+          expect(price.high).to.be.a('number')
+          expect(price.closing).to.be.a('number')
+          expect(price.pricedAt).to.be.a('string')
+
+          expect(isDateISO8601(price.pricedAt)).to.equal(true)
+        })
+
+        done()
+      })
+  })
+})
