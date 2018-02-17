5867ea12f805a1d602092434b40cb8e737b4f761
Marco Aurelio Maturana Silveiro <marco.maturana.ro@gmail.com>
2018-02-17 17:58:22 -0400

    Add default port server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 let app = require('./config/server')
 
-const port = process.env.APP_PORT
+const port = process.env.APP_PORT || 3000
 
 app.listen(port, () => {
   console.log('Server listening on port ' + port + '!')
