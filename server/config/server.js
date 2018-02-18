b6eb311692023c25a66ed65e12ea60313c8ab2b7
Marco Aurelio Maturana Silveiro <marco.maturana.ro@gmail.com>
2018-02-18 17:27:25 -0400

    Add CORS and refact server config
    Add CORS to accept client request

--- server/config/server.js
+++ server/config/server.js
@@ -1,10 +1,11 @@
 let
   bodyParser = require('body-parser'),
   consign = require('consign'),
-  compression = require('compression')
-  dotenv = require('dotenv')
-  express = require('express')
-  helmet = require('helmet')
+  compression = require('compression'),
+  dotenv = require('dotenv'),
+  express = require('express'),
+  helmet = require('helmet'),
+  cors = require('cors'),
   path = require('path')
 
 let app = express()
@@ -13,6 +14,7 @@ dotenv.config({path: path.join(__dirname,'../.env')})
 
 app.use(helmet())
 app.use(compression())
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
