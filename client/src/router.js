38c5ad90088a18052e4dd7354a4fe26d907dc793
Marco Aurelio Maturana Silveiro <marco.maturana.ro@gmail.com>
2018-02-18 17:05:02 -0400

    Route configuration

--- client/src/router.js
+++ client/src/router.js
@@ -4,30 +4,25 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 function load (component) {
-  // '@' is aliased to src/components
-  return () => import(`@/${component}.vue`)
+  return () => import(`views/${component}.vue`)
 }
 
 export default new VueRouter({
-  /*
-   * NOTE! VueRouter "history" mode DOESN'T works for Cordova builds,
-   * it is only to be used only for websites.
-   *
-   * If you decide to go with "history" mode, please also open /config/index.js
-   * and set "build.publicPath" to something other than an empty string.
-   * Example: '/' instead of current ''
-   *
-   * If switching back to default "hash" mode, don't forget to set the
-   * build publicPath back to '' so Cordova builds work again.
-   */
-
-  mode: 'hash',
-  scrollBehavior: () => ({ y: 0 }),
-
+  mode: 'history',
   routes: [
-    { path: '/', component: load('Hello') },
-
-    // Always leave this last one
-    { path: '*', component: load('Error404') } // Not found
+    {
+      path: '*',
+      component: load('layouts/default'),
+      children: [
+        {
+          path: '/portfolio',
+          component: load('pages/stocks/portfolio')
+        },
+        {
+          path: '/history',
+          component: load('pages/stocks/history')
+        }
+      ]
+    }
   ]
 })
