d1438cfb84fd3412a1e4f3806fc782312973af1b
Leonardo Santos <leon.sdsilva@gmail.com>
2020-02-07 20:50:12 -0300

    chore: added validator middleware to history and compare

--- node/src/services/StocksService.ts
+++ node/src/services/StocksService.ts
@@ -16,16 +16,32 @@ class StocksService {
 
   public async fetchRecentQuotation(stockName: string): Promise<Quotation> {
     const { data } = await this.client.get(`query?function=GLOBAL_QUOTE&symbol=${stockName}&apikey=${process.env.ALPHAVANTAGE_KEY}`)
-    const quotation = transformQuotation(data)
 
-    return quotation
+    if (this.successfulResponse(data)) {
+      const quotation = transformQuotation(data)
+      return quotation
+    }
+
+    throw new Error(this.getErrorMessage(data))
   }
 
   public async fetchHistoricalSharePrice(stockName: string, from: string, to: string): Promise<History> {
     const { data } = await this.client.get(`query?function=TIME_SERIES_DAILY&symbol=${stockName}&apikey=${process.env.ALPHAVANTAGE_KEY}&outputsize=full`)
-    const historical = transformHistoricalPrice(data, from, to)
 
-    return historical
+    if (this.successfulResponse(data)) {
+      const historical = transformHistoricalPrice(data, from, to)
+      return historical
+    }
+
+    throw new Error(this.getErrorMessage(data))
+  }
+
+  private successfulResponse(data): boolean {
+    return !data.Note && !data['Error Message']
+  }
+
+  private getErrorMessage(data): string {
+    return data.Note || data['Error Message'] || 'Unknown error'
   }
 }
 
