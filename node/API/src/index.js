c450c8ee1777a3545bccd7a9c601b056477ca4c0
João Rizzo <joaorizzont@hotmail.com>
2020-07-22 09:30:29 -0300

    fix(estrutura): Corrigido estrutura do projeto

--- node/API/src/index.js
+++ node/API/src/index.js
@@ -1,14 +1,28 @@
-const axios = require("axios");
-const { API_KEY } = require('../src/configs/config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
 
-const api = axios.create({
-    baseURL: "https://www.alphavantage.co"
+const PORT = process.env.PORT || 3333
+const app = express();
+
+
+app.use(cors({ origin: true }))
+
+
+//Para o express entender informaçoes em json
+app.use(bodyParser.json());
+
+//Para entender URL por parametros
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+//Importando todos os controllers
+//Importando todos os controllers
+require('./controllers/index.js')(app);
+
+
+//Iniciando o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}!`);
 });
 
-//Adiciona a chave da api antes de fazer a chamada a api da alphavantage
-api.interceptors.request.use(async config => {
-    config.url += `&apikey=${API_KEY}`
-    return config;
-});
-
-module.exports = api
---
+++ node/API/src/services/AlphaApi.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+const { API_KEY } = require('../configs/config');
+
+const api = axios.create({
+    baseURL: "https://www.alphavantage.co"
+});
+
+//Adiciona a chave da api antes de fazer a chamada a api da alphavantage
+api.interceptors.request.use(async config => {
+    config.url += `&apikey=${API_KEY}`
+    return config;
+});
+
+module.exports = api
