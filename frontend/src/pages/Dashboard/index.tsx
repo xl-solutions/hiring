3790ff8de02183daee21f9223565f1085cb4e493
Marcos Corsi <marcosvcorsi@gmail.com>
2020-07-22 21:27:33 -0300

    Dashboard finished

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,12 +1,131 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { FiTrendingUp, FiBarChart2, FiX, FiPlus } from 'react-icons/fi';
 import Header from '../../components/Header';
 
-import { Container } from './styles';
+import { Container, StockList, StockItem, SearchContainer } from './styles';
+
+import formatValue from '../../utils/formatValue';
+import formatDate from '../../utils/formatDate';
+
+import api from '../../services/api';
+
+interface StockInfo {
+  name: string;
+  lastPrice: number;
+  priceAt: string;
+}
 
 const Dashboard: React.FC = () => {
+  const [stockNames, setStockNames] = useState<string[]>([]);
+
+  const [loading, setLoading] = useState(false);
+  const [stockText, setStockText] = useState('');
+  const [stocks, setStocks] = useState<StockInfo[]>([]);
+
+  useEffect(() => {
+    async function loadStocks(): Promise<void> {
+      const storeStockNames = localStorage.getItem('@Finances:stockNames');
+
+      if (storeStockNames) {
+        const parsedStockNames: string[] = JSON.parse(storeStockNames);
+        const localStocks: StockInfo[] = [];
+
+        for (const stockName of parsedStockNames) {
+          const { data } = await api.get<StockInfo>(
+            `/stocks/${stockName}/quote`,
+          );
+
+          localStocks.push(data);
+        }
+
+        setStocks(localStocks);
+        setStockNames(parsedStockNames);
+      }
+    }
+
+    loadStocks();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('@Finances:stockNames', JSON.stringify(stockNames));
+  }, [stockNames]);
+
+  const handleAddStock = useCallback(
+    async e => {
+      e.preventDefault();
+
+      if (stockText) {
+        try {
+          setLoading(true);
+
+          const { data } = await api.get<StockInfo>(
+            `/stocks/${stockText}/quote`,
+          );
+
+          setStocks([...stocks, data]);
+          setStockNames([...stockNames, stockText]);
+          setStockText('');
+        } catch (err) {
+          alert('Ops, algo deu errado, verifique o nome da ação');
+        } finally {
+          setLoading(false);
+        }
+      }
+    },
+    [stocks, stockText, stockNames],
+  );
+
+  const handleRemoveStock = useCallback(stockName => {
+    setStocks(state => state.filter(stock => stock.name !== stockName));
+    setStockNames(state => state.filter(stock => stock !== stockName));
+  }, []);
+
   return (
     <>
       <Header />
+      <Container>
+        <SearchContainer>
+          <form onSubmit={handleAddStock}>
+            <input
+              type="text"
+              placeholder="Digite o nome da ação"
+              value={stockText}
+              onChange={e => setStockText(e.target.value)}
+            />
+            <button type="submit" disabled={loading}>
+              <FiPlus />
+              Adicionar
+            </button>
+          </form>
+        </SearchContainer>
+
+        <StockList>
+          {stocks.map(stock => (
+            <StockItem key={stock.name}>
+              <strong>{stock.name}</strong>
+              <p>
+                {formatValue(stock.lastPrice)}
+                <span>
+                  (Atualizado em {formatDate(new Date(stock.priceAt))})
+                </span>
+              </p>
+
+              <div className="action-buttons">
+                <button type="button">
+                  <FiBarChart2 />
+                  Historico
+                </button>
+                <button type="button">
+                  <FiTrendingUp />
+                  Projeção
+                </button>
+              </div>
+
+              <FiX onClick={() => handleRemoveStock(stock.name)} />
+            </StockItem>
+          ))}
+        </StockList>
+      </Container>
     </>
   );
 };
--- frontend/src/pages/Dashboard/styles.ts
+++ frontend/src/pages/Dashboard/styles.ts
@@ -1,3 +1,127 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 40px 20px;
+`;
+
+export const SearchContainer = styled.div`
+  form {
+    display: flex;
+    align-items: center;
+
+    input {
+      padding: 12px;
+      border-radius: 8px;
+      outline: 0;
+      border: 0;
+      width: 300px;
+      margin-right: 3px;
+    }
+
+    button {
+      padding: 10px;
+      margin: 3px;
+      outline: 0;
+      border: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #6001d2;
+      color: #fff;
+      border-radius: 8px;
+      transition: 0.2s;
+      width: 125px;
+
+      svg {
+        margin-right: 5px;
+      }
+
+      &:hover {
+        opacity: 0.9;
+      }
+    }
+  }
+`;
+
+export const StockList = styled.ul`
+  margin-top: 30px;
+  list-style: none;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
+
+export const StockItem = styled.li`
+  padding: 15px;
+  border: 0.5px solid #333;
+  border-radius: 10px;
+  position: relative;
+  background-color: #eee;
+  transition: 0.2s;
+
+  > strong {
+    font-size: 20px;
+    font-weight: 500px;
+  }
+
+  > p {
+    margin-top: 8px;
+    font-size: 16px;
+
+    span {
+      margin-left: 3px;
+      font-size: 14px;
+    }
+  }
+
+  > div {
+    margin-top: 10px;
+    display: flex;
+    align-items: center;
+
+    button {
+      padding: 10px;
+      margin: 3px;
+      outline: 0;
+      border: 0;
+      display: flex;
+      align-items: center;
+      background-color: #6001d2;
+      color: #fff;
+      border-radius: 5px;
+      transition: 0.2s;
+
+      svg {
+        margin-right: 5px;
+      }
+
+      &:hover {
+        opacity: 0.9;
+      }
+    }
+  }
+
+  & + li {
+    margin-left: 10px;
+  }
+
+  > svg {
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin-top: 5px;
+    margin-right: 8px;
+    cursor: pointer;
+    transition: 0.2s;
+
+    &:hover {
+      color: #333;
+    }
+  }
+
+  &:hover {
+    transform: translateY(-3px);
+  }
+`;
