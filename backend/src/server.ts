7de70d304b2cdba9860006d1b13078cdb5946a22
Marcos Corsi <marcosvcorsi@gmail.com>
2020-07-24 22:25:06 -0300

    Bug fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,5 @@
 import app from './app';
 
-app.listen(3333, () => console.log('Server is running on port 3333'));
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
