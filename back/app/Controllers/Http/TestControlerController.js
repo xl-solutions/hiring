62d8f789becd1e43068d8384d5ac6aaa1216726e
Allison Piovani <apiovani25@gmail.com>
2020-07-25 22:06:36 -0300

    fix routes

--- back/app/Controllers/Http/TestControlerController.js
+++ back/app/Controllers/Http/TestControlerController.js
@@ -3,76 +3,30 @@
 const Env = use('Env');
 const axios = use('axios');
 
-const b3 = [
-    'AZUL4',
-    'ABEV3',
-    'BTOW3',
-    'B3SA3',
-    'BBSE3',
-    'VRML3',
-    'BBDC4',
-    'BBDC3',
-    'BRAP4',
-    'BBAS3',
-    'BRKM5',
-    'BRFS3',
-    'CCRO3',
-    'CMIG4',
-    'CIEL3',
-    'CSAN3',
-    'CVCB3',
-    'CYRE3',
-    'ECOR3',
-    'ELET3',
-    'ELET6',
-    'EMBR3',
-    'ENBR3',
-    'ENGIE3',
-    'EQTL3',
-    'YDUQ3',
-    'FLRY3',
-    'GGBR4',
-    'GOAU4',
-    'GOLL4',
-    'HYPE3',
-    'IGTA3',
-    'IRBR3',
-    'ITSA4',
-    'ITUB4',
-    'JBSS3',
-    'KLBN11',
-    'KROT3',
-    'RENT3',
-    'LAME4',
-    'LREN3',
-    'MGLU3',
-    'MRFG3',
-    'MRVE3',
-    'MULT3',
-    'NATU3',
-    'PCAR4',
-    'PETR4',
-    'PETR3',
-    'BRDT3',
-    'QUAL3',
-    'RADL3',
-    'RAIL3',
-    'SBSP3',
-    'SANB11',
-    'CSNA3',
-    'SMLS3',
-    'SUZB5',
-    'TAEE11',
-    'VIVT4',
-    'TIMP3',
-    'UGPA3',
-    'USIM5',
-    'VALE3',
-    'VVAR3',
-    'WEGE3',
-];
-
 class TestControlerController {
+
+    async quote({request, response}) {
+        const stockName = request.params.stock_name;
+        let stock = {};
+
+        await axios.get(Env.get('ALPHAVANTAGE_URL'), {
+            params: {
+                'apikey': Env.get('ALPHAVANTAGE_KEY'),
+                'function': 'GLOBAL_QUOTE',
+                'symbol': stockName,
+            }
+        }).then(({data}) => {
+            const temp = data['Global Quote'];
+
+            stock = {
+                name: temp['01. symbol'],
+                lastPrice: temp['01. symbol'],
+                pricedAt: temp['05. price']
+            };
+        });
+
+        return response.send(stock);
+    }
 
     async index({request}) {
 
@@ -86,26 +40,6 @@ class TestControlerController {
         }).then((response) => {
             console.log(response);
         });
-    }
-
-    async search({request, response}) {
-        const search = request.get().search;
-
-        await axios.get(Env.get('ALPHAVANTAGE_URL'), {
-            params: {
-                'apikey': Env.get('ALPHAVANTAGE_KEY'),
-                'function': 'SYMBOL_SEARCH',
-                'keywords': search,
-            }
-        }).then(({data}) => {
-            return response.send(data.bestMatches.map((element) => {
-                return {
-                    'initials': element['1. symbol'],
-                    'name': element['2. name'],
-                };
-            }));
-        }).catch(() => {
-            console.log('DEU RUIM');        });
     }
 
     async value({request, response}) {
@@ -124,6 +58,46 @@ class TestControlerController {
         }).catch(() => {
             console.log('DEU RUIM');
         });
+    }
+
+    async values({request, response}) {
+        const sao = '.SAO';
+        const nameRemove = request.get().name;
+        const nameWithoutSao = nameRemove.replace(sao, '');
+        let res = [];
+
+        const newB3 = b3.filter((element) => {
+            return element != nameWithoutSao;
+        });
+
+        newB3.forEach(element => {
+            let value = 0.00;
+
+            axios.get(Env.get('ALPHAVANTAGE_URL'), {
+                params: {
+                    'apikey': Env.get('ALPHAVANTAGE_KEY'),
+                    'function': 'GLOBAL_QUOTE',
+                    'symbol': element.concat(sao),
+                }
+            }).then(({data}) => {
+                const action = data['Global Quote'];
+
+                value = action['05. price'];
+            }).catch(() => {});
+
+            res.push({
+                name: element,
+                value: value
+            });
+        });
+
+        return response.send(res);
+    }
+
+    async historic({request, response}) {
+        const name = request.get().name;
+
+        return response.send('AQUI');
     }
 }
 
--- back/start/routes.js
+++ back/start/routes.js
@@ -6,6 +6,17 @@ const Route = use('Route')
 
 Route.get('/', 'TestControlerController.index');
 
+Route.group(() => {
+
+    Route.get('/:stock_name/quote', 'TestControlerController.quote');
+
+}).prefix('/stocks')
+
+
 Route.get('/search', 'TestControlerController.search');
 
 Route.get('/value', 'TestControlerController.value');
+
+Route.get('/values', 'TestControlerController.values');
+
+Route.get('/historic', 'TestControlerController.historic');
