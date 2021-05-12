1e0b95ac2e1cd4798d967ba9497922bab729109e
Fredson Chaves <fredsonchaves07@gmail.com>
2021-05-12 13:42:22 -0300

    feat: Creation of the initial structure of the controlle

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask 
 from app import config
 
+
 def create_app():
     app = Flask(__name__)
     config.init_app(app)
--- app/resources/main.py
+++ app/resources/main.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, render_template
+from app.controller import product_controller
+
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
+    products = product_controller.list()
+    
     return render_template('index.html')
