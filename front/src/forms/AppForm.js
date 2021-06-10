5c0d769a8024c608d3b98b014e57715a65460a9d
Marco Gomes <marcoaduartegomes@gmail.com>
2021-06-10 15:44:12 -0300

    Fixes
    Form and validation

--- front/src/forms/AppForm.js
+++ front/src/forms/AppForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { Formik, Form as FormikForm } from 'formik'
+import { Formik, Form as FormikForm, ErrorMessage } from 'formik'
 
 class AppForm extends Component {
   constructor(props) {
@@ -12,6 +12,7 @@ class AppForm extends Component {
       <Formik
         initialValues={this.props.initialValues}
         onSubmit={this.props.handleSubmit}
+        validationSchema={this.props.validationSchema}
       >
         {() => <FormikForm>{this.props.children}</FormikForm>}
       </Formik>
