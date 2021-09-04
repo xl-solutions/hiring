154cca0adf5d8f21f9fa62705d21f9e2ede1bcd6
lucaspedronet <lucaspedrolbg@gmail.com>
2021-09-04 02:03:08 -0300

    [Font, AppLoading] setting initial

--- App.tsx
+++ App.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import AppLoading from 'expo-app-loading';
+
+import {
+  useFonts,
+  IBMPlexSans_400Regular,
+  IBMPlexSans_600SemiBold,
+  IBMPlexSans_500Medium,
+  IBMPlexSans_700Bold,
+} from '@expo-google-fonts/ibm-plex-sans';
+
+function App() {
+  const [fontLoading] = useFonts({
+    IBMPlexSans_600SemiBold,
+    IBMPlexSans_400Regular,
+    IBMPlexSans_500Medium,
+    IBMPlexSans_700Bold,
+  });
+
+  if (!fontLoading) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Container>
+
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export { App };
