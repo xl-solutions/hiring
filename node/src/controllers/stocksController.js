d7a45829ab250e213ae5b2f48f1b8c953f8566ab
dkme01 <dkme01@gmail.com>
2021-09-07 19:53:54 -0300

    Stock history route

--- node/src/controllers/stocksController.js
+++ node/src/controllers/stocksController.js
@@ -19,17 +19,82 @@ async function getStockQuote(request, response) {
       return response.status(400).send({ erro: "Limite de busca por minuto atingido" });
     }
 
-    const response = {
+    const stock = {
       name: stock_name,
       lastPrice: parseFloat(data["Global Quote"]["05. price"]),
       pricedAt: data["Global Quote"]["07. latest trading day"],
     };
 
-    return response.status(200).send(response);
+    return stock.status(200).send(stock);
   } catch (error) {
     console.error(error);
     return response.status(400).send({ erro: error.toString() });
   }
 }
 
-module.exports = { getStockQuote };
+async function getStockHistory(request, response) {
+  try {
+    const { from, to } = request.query;
+    const { stock_name } = request.params;
+
+    if (!stock_name || stock_name === "" || !from || from === "" || !to || to === "")
+      return response.status(400).send({ erro: "Argumentos invalidos" });
+
+    const fromSplit = from.split("-");
+
+    if (fromSplit.length !== 3 || fromSplit[0].length !== 4 || fromSplit[1].length !== 2 || fromSplit[2].length !== 2) {
+      return response.status(400).send({ erro: "Data inicial com formato invalido. Esperado: yyyy-mm-dd" });
+    }
+
+    const toSplit = to.split("-");
+
+    if (toSplit.length != 3 || toSplit[0].length != 4 || toSplit[1].length != 2 || toSplit[2].length != 2) {
+      return response.status(400).send({ erro: "Data final com formato invalido. Esperado: yyyy-mm-dd" });
+    }
+
+    if (new Date(from) > new Date(to)) {
+      return response.status(400).send({ erro: "Data inicial deve ser menor que a final." });
+    }
+
+    const { data } = await api.get(`/query?function=TIME_SERIES_DAILY&symbol=${stock_name}&outputsize=full`)[
+      "Time Series (Daily)"
+    ];
+
+    if (!data) {
+      return response.status(404).send({ erro: `Ação ${stock_name} não encontrada` });
+    }
+
+    if (data.Note) {
+      return response.status(400).send({ erro: `Limite de Busca na API alphavantage por minuto atingido` });
+    }
+
+    const dataKeys = Object.keys(data);
+
+    let stock = {
+      name: stock_name,
+      prices: [],
+    };
+
+    let last = "";
+
+    let first = dataKeys.indexOf(from) == -1 ? 0 : dataKeys.indexOf(from);
+
+    for (i = first; last != to && i != 0; i--) {
+      stock.prices.push({
+        opening: parseFloat(data[dataKeys[i]]["1. open"]),
+        low: parseFloat(data[dataKeys[i]]["3. low"]),
+        high: parseFloat(data[dataKeys[i]]["2. high"]),
+        closing: parseFloat(data[dataKeys[i]]["4. close"]),
+        pricedAt: dataKeys[i],
+      });
+      last = dataKeys[i];
+    }
+
+    return response.send(stock);
+  } catch (error) {
+    console.log(error);
+    return response.status(400).send({ erro: error.toString() });
+  }
+}
+
+module.exports = { getStockQuote, getStockHistory };
--- node/src/routes.js
+++ node/src/routes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const { getStockQuote } = require("../src/controllers/stocksController");
+const { getStockQuote, getStockHistory } = require("../src/controllers/stocksController");
 
 const routes = Router();
 
 routes.get("/:stock_name/quote", getStockQuote);
+routes.get("/:stock_name/history", getStockHistory);
 
 module.exports = routes;
