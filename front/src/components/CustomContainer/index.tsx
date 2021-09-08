9a47f33b8d385d0d7a92d760b0c3dac808353200
dkme01 <dkme01@gmail.com>
2021-09-08 15:40:04 -0300

    container component

---
+++ front/src/components/CustomContainer/index.tsx
@@ -0,0 +1,18 @@
+import { Container, Grid, Paper } from "@material-ui/core";
+import { useStyles } from "../../globals/styles";
+
+function CustomContainer({ children }: any) {
+  const { content, container, paper } = useStyles();
+
+  return (
+    <main className={content}>
+      <Container maxWidth="lg" className={container}>
+        <Grid container spacing={2} direction="row" justify="center">
+          <Paper className={paper}>{children}</Paper>
+        </Grid>
+      </Container>
+    </main>
+  );
+}
+
+export default CustomContainer;
---
+++ front/src/pages/main/index.tsx
@@ -0,0 +1,5 @@
+import { useEffect, useState } from "react";
+import {} from "@material-ui/core";
+import { api } from "../../services/api";
+
+export function Main() {}
