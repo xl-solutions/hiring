08e75554caa656450c2b723010ee92d69956bc47
luiz <luizrosalba@gmail.com>
2021-10-27 11:34:05 -0300

    feat(frontend): Adding a colored background route selector to header
    Adding a colored background route selector to header

--- node/src/common/template/header.js
+++ node/src/common/template/header.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState }  from "react";
 
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 
 //import logo from '../../assets/logo.png'
+import '../../App.css';
 
-const Header = () => {
+const URLhas= () =>{
+    return window.location.pathname
+}
+const Header = (props) => {
+
     return(
        <Navbar bg="white" expand="lg">
         <Container>
@@ -19,11 +24,33 @@ const Header = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link href="search">Procurar Ação</Nav.Link>
-                <Nav.Link href="quote">Cotação</Nav.Link>
-                <Nav.Link href="history">Histórico</Nav.Link>
-                <Nav.Link href="compare">Comparar</Nav.Link>
-                <Nav.Link href="project">Projetar</Nav.Link>
+                <Nav.Link 
+                    className={URLhas().includes("search")?"header-selected":null} 
+                    href="search"
+                    >Procurar Ação
+                </Nav.Link>
+                <Nav.Link 
+                    className={URLhas().includes("quote")?"header-selected":null} 
+                    href="quote"
+                    >Cotação
+                </Nav.Link>
+                <Nav.Link 
+                    href="history"
+                    className={URLhas().includes("history")?"header-selected":null} 
+                    >Histórico
+                </Nav.Link>
+                <Nav.Link 
+                    href="compare"
+                    className={URLhas().includes("compare")?"header-selected":null} 
+                    >
+                        Comparar
+                </Nav.Link>
+                <Nav.Link 
+                    href="project"
+                    className={URLhas().includes("project")?"header-selected":null} 
+                    >
+                        Projetar
+                </Nav.Link>
             </Nav>
             </Navbar.Collapse>
         </Container>
