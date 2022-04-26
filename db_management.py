77d33d7a2ff8eb5de06971975dc35aab5d3b7c88
Piske <marcelopiske@ideyas.com>
2022-04-25 23:55:59 -0300

    subindo primeira parte do front end listando tabelas

---
+++ db_management.py
@@ -0,0 +1,74 @@
+import psycopg2
+
+def validate_database() -> bool:
+    is_valid = True
+
+    try:
+        con = psycopg2.connect(
+            host='localhost',
+            database='postgres',
+            user='postgres',
+            password='2208'
+        )
+        cur = con.cursor()
+
+    except Exception as e:
+        is_valid = False
+        print(f"Não conectado {e}")
+
+    try:
+        sql = 'CREATE TABLE IF NOT EXISTS public.telefones (id serial primary key, manufacturer varchar(50), model varchar(50), color varchar(25), carrier_plan_type varchar(5), quantity int, price float )'
+        cur.execute(sql)
+        con.commit()
+    except Exception as e:
+        is_valid = False
+        print(e)
+    
+    if is_valid:
+        client = {
+            'is_valid': is_valid,
+            'con': con,
+            'cur': cur
+        }
+    else:
+        client = {
+            'is_valid': is_valid
+        }
+    return client
+
+def create_data(data): # is_ok [True: ok, 1: erro de conexao, 2: falha ao criar ou encontra tabela, 3: falha ao inserir itens]
+    is_ok = True
+    client = validate_database()
+    cur = client.get('cur')
+    con = client.get('con')
+    is_valid = client.get('is_valid')
+    print(client)
+    if is_valid:
+        for d in data:
+            sql = f"""INSERT INTO telefones values (default, '{d.get("manufacturer")}', '{d.get("model")}', '{d.get("color")}', '{d.get("carrier_plan_type")}', '{d.get("quantity")}', '{d.get("price")}')"""
+            cur.execute(sql)
+            con.commit()
+        con.close()
+    else:
+        is_ok = False
+        print("Banco de dados nao validado")
+
+    return is_ok
+
+
+def get_data():
+    client = validate_database()
+    cur = client.get('cur')
+    con = client.get('con')
+    is_valid = client.get('is_valid')
+    
+    if is_valid:
+        sql = 'SELECT * FROM public.telefones'
+        cur.execute(sql)
+        rows = cur.fetchall()
+        con.close()
+        return rows
+    else:
+        print("erro")
+
+    return rows
--- index.py
+++ index.py
@@ -1,24 +1,53 @@
+import json
 import os
-from flask import Flask, flash, request, redirect, url_for, render_template
-from werkzeug.utils import secure_filename
+from db_management import create_data, get_data
+from utils import format_json
+from flask import Flask, flash, request, redirect, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
+    
+    return render_template('index.html')
+
+@app.route('/create', methods=['POST'])
+def create_itens():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'csv' not in request.files:
             flash('No file part')
             return redirect(request.url)
+        
         file = request.files.get('csv') #arquivo csv
         
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         
-    print(request.files.get('csv'))
-    return render_template('index.html')
+        try:
+            df = pd.read_csv(file)
+            df = json.loads(df.to_json())
+        except:
+            flash("por favor selecone um cvs")
+        
+        else:
+            formated_json = format_json(df)
+            is_created = create_data(formated_json)
+            if is_created:
+                print("dados criados com sucesso")
+            else:
+                print("erro ao criar dados")
+
+@app.route('/get_itens', methods=['GET'])
+def get_itens():
+    itens = get_data()
+    print(itens)
+    df = {
+        'itens': itens
+    }
+    return json.dumps(df)
 
 if __name__ == "__main__":
     app.run()
