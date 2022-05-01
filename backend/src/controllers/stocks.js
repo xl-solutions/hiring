512c9ff7c697a0aedf9bcb4c4d36dd61372b5129
joserx <jose.hamilton2050@gmail.com>
2022-05-01 01:03:14 -0300

    add error handling and price history

--- backend/src/controllers/stocks.js
+++ backend/src/controllers/stocks.js
@@ -27,3 +27,86 @@ async function getStocks(req, res) {
         return res.status(400).send({ erro: error.toString() });
     }
 }
+
+async function getHistoryStock(req, res) {
+    try {
+        const { stock_name } = req.params;
+        const { to, from } = req.query;
+
+        if (!stock_name) {
+            return res.status(400).send("Nome da ação inválida");
+        }
+
+        if (!to || !from) {
+            return res.status(400).send("Data inválida");
+        }
+
+        const toSplit = to.split("-");
+        const fromSplit = from.split("-");
+
+        if (
+            toSplit.length !== 3 ||
+            toSplit["0"].length !== 4 ||
+            toSplit["1"].length !== 2 ||
+            toSplit["2"].length !== 2
+        ) {
+            return res
+                .status(400)
+                .send("Insira o formato de data válido: YYYY-MM-DD");
+        }
+
+        if (
+            fromSplit.length !== 3 ||
+            fromSplit["0"].length !== 4 ||
+            fromSplit["1"].length !== 2 ||
+            fromSplit["2"].length !== 2
+        ) {
+            return res
+                .status(400)
+                .send("Insira o formato de data válido: YYYY-MM-DD");
+        }
+
+        if (new Date(from) > new Date(to)) {
+            return res
+                .status(400)
+                .send("A data final não pode ser menor que a inicial!");
+        }
+
+        const { data } = await api.get(
+            `/query?function=TIME_SERIES_DAILY&symbol=${stock_name}&outputsize=full&apikey=${process.env.API_KEY}`
+        );
+
+        if (!data) {
+            return res.status(404).send("Ação não encontrada");
+        }
+
+        const Keys = Object.keys(data["Time Series (Daily)"]);
+
+        const StockHistory = {
+            name: stock_name,
+            prices: [],
+        };
+
+        let last = "";
+        let first = Keys.indexOf(from) === -1 ? 0 : Keys.indexOf(from);
+
+        for (let i = first; last !== to && i !== 0; i--) {
+            if (data["Time Series (Daily)"][Keys[i]]) {
+                StockHistory.prices.push({
+                    opening: parseFloat(data["Time Series (Daily)"][Keys[i]]["1. open"]),
+                    low: parseFloat(data["Time Series (Daily)"][Keys[i]]["3. low"]),
+                    high: parseFloat(data["Time Series (Daily)"][Keys[i]]["2. high"]),
+                    closing: parseFloat(data["Time Series (Daily)"][Keys[i]]["4. close"]),
+                    pricedAt: Keys[i],
+                });
+                last = Keys[i];
+            }
+        }
+
+        return res.status(200).send(StockHistory);
+    } catch (error) {
+        return res.status(400).send({ erro: error.toString() });
+    }
+}
+
+module.exports = { getStocks, getHistoryStock };
