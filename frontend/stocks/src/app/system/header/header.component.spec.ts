1f6d32516bc7a1272bf3554b656bb1c89a9011ec
joserx <jose.hamilton2050@gmail.com>
2022-05-02 13:06:13 -0300

    added unit test in header component

--- frontend/stocks/src/app/system/header/header.component.spec.ts
+++ frontend/stocks/src/app/system/header/header.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+import {By} from '@angular/platform-browser'
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -21,5 +23,22 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit header title name on click', () => {
+    component.items = [{ title: 'Meu portifólio' },
+                        { title: 'Ações' },
+                        { title: 'Histórico de greços' },
+                        { title: 'Projeção de ganhos'},
+                        { title: 'Comparar ações'}];
+    fixture.detectChanges();
+
+    const firstName = fixture.debugElement.query(By.css('#title a'));
+
+    component.response.subscribe((response) => {
+      expect(response).toEqual({ title: 'Meu portifólio', selected: true });
+    });
+
+    firstName.triggerEventHandler('click', null);
   });
 });
