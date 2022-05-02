c4ea317acfd6ecc87146d1cdb8e282d464d7054c
joserx <jose.hamilton2050@gmail.com>
2022-05-02 04:03:55 -0300

    added basic styling

--- frontend/stocks/src/app/system/stocks/stocks.component.ts
+++ frontend/stocks/src/app/system/stocks/stocks.component.ts
@@ -20,10 +20,7 @@ export class StocksComponent implements OnInit {
   }
 
   async getStock() {
-    console.log(this.stockName);
-
     const result = await this.stocksService.getStockByName(this.stockName);
     this.stock = result;
-    console.log(result);
   }
 }
