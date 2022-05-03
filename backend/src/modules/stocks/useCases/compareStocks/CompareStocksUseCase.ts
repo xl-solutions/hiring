ba85e37cdf49a282763fdfae307b42e7bf982035
jfdsouza988 <juliosouza.rs@gmail.com>
2022-05-03 11:46:24 -0300

    added route for compare stocks

---
+++ backend/src/modules/stocks/useCases/compareStocks/CompareStocksUseCase.ts
@@ -0,0 +1,47 @@
+import {
+  getQuoteBySymbol,
+  GlobalQuote,
+} from '../../../../services/getQuoteBySymbol';
+import { AppError } from '../../../../shared/errors/AppError';
+
+interface IRequest {
+  stock_name: string;
+  stocks: string[];
+}
+
+interface IReturn {
+  lastPrices: ILastPrice[];
+}
+
+interface ILastPrice {
+  name: string;
+  lastPrice: number;
+  pricedAt: string;
+}
+
+export class CompareStocksUseCase {
+  async execute({ stock_name, stocks }: IRequest) {
+    const lastPrices: ILastPrice[] = [];
+
+    if (!stock_name) {
+      throw new AppError('Stock name not provided');
+    }
+
+    stocks.unshift(stock_name);
+    const searchQuotes = (await getQuoteBySymbol(stocks)) as GlobalQuote[];
+
+    searchQuotes.forEach((stock) => {
+      lastPrices.push({
+        name: stock['01. symbol'],
+        lastPrice: Number(stock['05. price']),
+        pricedAt: stock['07. latest trading day'],
+      });
+    });
+
+    const compareStocks: IReturn = {
+      lastPrices,
+    };
+
+    return compareStocks;
+  }
+}
--- backend/src/shared/routes/stocks.routes.ts
+++ backend/src/shared/routes/stocks.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { CompareStocksController } from '../../modules/stocks/useCases/compareStocks/CompareStocksController';
 import { GetLastQuoteController } from '../../modules/stocks/useCases/getLastQuote/GetLastQuoteController';
 import { HistoricalPriceByDateController } from '../../modules/stocks/useCases/historicalPriceByDate/HistoricalPriceByDateController';
 
@@ -6,11 +7,13 @@ const stocksRoutes = Router();
 
 const getLastQuoteController = new GetLastQuoteController();
 const historicalPriceByDateController = new HistoricalPriceByDateController();
+const compareStocksController = new CompareStocksController();
 
 stocksRoutes.get('/:stock_name/quote', getLastQuoteController.handle);
 stocksRoutes.get(
   '/:stock_name/history',
   historicalPriceByDateController.handle,
 );
+stocksRoutes.post('/:stock_name/compare', compareStocksController.handle);
 
 export { stocksRoutes };
