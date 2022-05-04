0d5fc5b7593c5e8ddd22419d8da44b82857c8b9c
jfdsouza988 <juliosouza.rs@gmail.com>
2022-05-04 14:20:21 -0300

    added route for search stocks in external api

---
+++ backend/src/modules/stocks/useCases/searchStocks/SearchStocksUseCase.ts
@@ -0,0 +1,34 @@
+import { searchStocksBySymbol } from '../../../../services/searchStocksBySymbol';
+import { AppError } from '../../../../shared/errors/AppError';
+
+interface IRequest {
+  symbol: string;
+}
+
+interface IResponse {
+  name: string;
+  company: string;
+  region: string;
+}
+
+export class SearchStocksUseCase {
+  async execute({ symbol }: IRequest) {
+    if (!symbol) {
+      throw new AppError('Search parameter not provided');
+    }
+
+    const searchStocks = await searchStocksBySymbol(symbol);
+
+    const stocks: IResponse[] = [];
+
+    searchStocks.forEach((stock) => {
+      stocks.push({
+        name: stock['1. symbol'],
+        company: stock['2. name'],
+        region: stock['4. region'],
+      });
+    });
+
+    return stocks;
+  }
+}
--- backend/src/shared/routes/stocks.routes.ts
+++ backend/src/shared/routes/stocks.routes.ts
@@ -3,6 +3,7 @@ import { CompareStocksController } from '../../modules/stocks/useCases/compareSt
 import { GainsByPurchaseOnSpecificDateController } from '../../modules/stocks/useCases/gainsByPurchaseOnSpecificDate/GainsByPurchaseOnSpecificDateController';
 import { GetLastQuoteController } from '../../modules/stocks/useCases/getLastQuote/GetLastQuoteController';
 import { HistoricalPriceByDateController } from '../../modules/stocks/useCases/historicalPriceByDate/HistoricalPriceByDateController';
+import { SearchStocksController } from '../../modules/stocks/useCases/searchStocks/SearchStocksController';
 
 const stocksRoutes = Router();
 
@@ -11,6 +12,7 @@ const historicalPriceByDateController = new HistoricalPriceByDateController();
 const compareStocksController = new CompareStocksController();
 const gainsByPurchasesController =
   new GainsByPurchaseOnSpecificDateController();
+const searchStocksController = new SearchStocksController();
 
 stocksRoutes.get('/:stock_name/quote', getLastQuoteController.handle);
 stocksRoutes.get(
@@ -18,6 +20,7 @@ stocksRoutes.get(
   historicalPriceByDateController.handle,
 );
 stocksRoutes.get('/:stock_name/gains', gainsByPurchasesController.handle);
+stocksRoutes.get('/search', searchStocksController.handle);
 stocksRoutes.post('/:stock_name/compare', compareStocksController.handle);
 
 export { stocksRoutes };
