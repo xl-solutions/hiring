d3c8accce1f8af433eae718442e5603ba7065d1d
jfdsouza988 <juliosouza.rs@gmail.com>
2022-05-06 18:32:27 -0300

    documenting the api with swagger

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,8 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import { AppError } from './shared/errors/AppError';
 import './shared/container/index';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from '../swagger.json';
 
 import { router } from './shared/routes';
 
@@ -14,6 +16,8 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
