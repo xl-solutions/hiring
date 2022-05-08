8f7e1fc8cba6a0f05e40b161faffcf129539a500
João Gabriel P. Pacheco <joao.prestupa@hotmail.com>
2022-05-08 05:05:12 -0300

    Model Product pronto

--- python/app/models/Product.py
+++ python/app/models/Product.py
@@ -1,10 +1,11 @@
-from database import db
+from app.database import db
 
 class Product(db.Model):
-    id_product = db.Column(db.Integer, primary_key=True, nullable=False)
-    manufacturer = db.Column(db.String(255), nullable=False)
-    model = db.Column(db.String(255), nullable=False)
-    color = db.Column(db.String(30), nullable=False)
-    carrier_plan_type = db.Column(db.String(3), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-    price = db.Column(db.Float, nullable=False)
+    product_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    product_manufacturer = db.Column(db.String(255), nullable=False)
+    product_model = db.Column(db.String(255), nullable=False)
+    product_color = db.Column(db.String(30), nullable=False)
+    product_carrier_plan_type = db.Column(db.String(3), nullable=False)
+    product_quantity = db.Column(db.Integer, nullable=False)
+    product_price = db.Column(db.Float, nullable=False)
+
