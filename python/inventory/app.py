72a1db85a2943c1d10348f7c3e1286fb998caa26
João Gabriel P. Pacheco <joao.prestupa@hotmail.com>
2022-05-08 19:29:28 -0300

    Arummando a interface e limpando o codigo

---
+++ python/inventory/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+from inventory import configuration
+from inventory import database
+from inventory import commands
+
+from inventory.routes.inventory_bp import inventory_bp
+
+
+def create_app():
+    app = Flask(__name__)
+
+    """ Adicionando  inicializando os modulos no objeto App"""
+    configuration.init_app(app)
+    database.init_app(app)
+    commands.init_app(app)
+
+    """ Adicionando a rotas no app """
+    app.register_blueprint(inventory_bp)
+
+    return app
