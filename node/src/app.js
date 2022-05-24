aa3b1e46eabce23942cbf96aed1613f36841e64d
matcontreras <76949314+matcontreras@users.noreply.github.com>
2022-05-24 14:14:12 -0300

    build: install cors to allow access from other localhost

--- node/src/app.js
+++ node/src/app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const server = express();
 const webRoutes = require('./routes/webRoutes');
 const path = require('path');
+const cors = require('cors');
 
 //settings
 server.set('views', path.join(__dirname, 'views'));
 
 //routes
+server.use(cors());
+server.listen(80, function () {
+    console.log('CORS-enabled web server listening on port 80')
+  })
 server.use(webRoutes);
 
 //middleware
