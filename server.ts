beb0959383f56181c4b6906afb7fd39df5eac162
AhhYeaah <vivi26743@hotmail.com>
2022-11-20 02:54:28 -0300

    extracting everything

--- server.ts
+++ server.ts
@@ -1,59 +1,39 @@
 import express from 'express';
 import cors from 'cors';
-import axios, { AxiosError } from 'axios';
+import { ApiService } from './src/services/ApiService';
+import { ApiController } from './src/controllers/ApiController';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-//thats dumb...
-interface YahooApiReponseResult {
-  symbol: string;
-  response: YahooApiResponseResponse[];
-}
+//Calling this here isents me of creating a controller instance in every api call
+const controller = new ApiController();
 
-interface YahooApiResponseResponse {
-  meta: {
-    regularMarketTime: number;
-    regularMarketPrice: number;
-  };
-}
+app.get('/stocks/:stock_name', async ({ params: { stock_name } }, res) => {
+  res.send(await controller.getStockBySymbol(stock_name));
+  // try {
+  //   const {
+  //     data: {
+  //       spark: { result },
+  //     },
+  //   } = await axios.get<YahooApiResponse>(
+  //     `https://query1.finance.yahoo.com/v7/finance/spark?symbols=${stock_name}&range=1d&interval=5m&indicators=close&includeTimestamps=false&includePrePost=false&corsDomain=finance.yahoo.com&.tsrc=finance`
+  //   );
 
-interface YahooApiResponse {
-  spark: {
-    result: YahooApiReponseResult[];
-    error: {
-      code: string;
-      description: string;
-    };
-  };
-}
-
-app.get('/getprecoAtual', async ({ body: { stock_name } }, res) => {
-  console.log(stock_name);
-
-  try {
-    const {
-      data: {
-        spark: { result },
-      },
-    } = await axios.get<YahooApiResponse>(
-      `https://query1.finance.yahoo.com/v7/finance/spark?symbols=${stock_name}&range=1d&interval=5m&indicators=close&includeTimestamps=false&includePrePost=false&corsDomain=finance.yahoo.com&.tsrc=finance`
-    );
-
-    if (result) {
-      const { regularMarketPrice, regularMarketTime } = result[0].response[0].meta!;
-      res.send({
-        name: result[0].symbol,
-        lastPrice: regularMarketPrice,
-        pricedAt: new Date(Number(regularMarketTime.toString() + '000')).toISOString(),
-      });
-    }
-  } catch (error: any) {
-    const errorObj = error.response.data.spark.error;
-    res.send(errorObj);
-  }
+  //   if (result) {
+  //     const { regularMarketPrice, regularMarketTime } = result[0].response[0].meta!;
+  //     res.send({
+  //       name: result[0].symbol,
+  //       lastPrice: regularMarketPrice,
+  //       pricedAt: new Date(Number(regularMarketTime.toString() + '000')).toISOString(),
+  //     });
+  //   }
+  // } catch (error: any) {
+  //   const errorObj = error.response.data.spark.error;
+  //   res.send(errorObj);
+  // }
 });
 
 app.listen(3000);
