52c8ab97c42300b2076788f6b0e712a6fd230e9a
AhhYeaah <vivi26743@hotmail.com>
2022-11-21 16:49:29 -0300

    cleaning server.ts

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import { ApiService } from './src/services/ApiService';
 import { ApiController } from './src/controllers/ApiController';
+import { ApiResponse } from './src/utils/types/ApiResponseFactory';
 
 const app = express();
 
@@ -11,29 +11,11 @@ app.use(cors());
 //Calling this here isents me of creating a controller instance in every api call
 const controller = new ApiController();
 
-app.get('/stocks/:stock_name', async ({ params: { stock_name } }, res) => {
-  res.send(await controller.getStockBySymbol(stock_name));
-  // try {
-  //   const {
-  //     data: {
-  //       spark: { result },
-  //     },
-  //   } = await axios.get<YahooApiResponse>(
-  //     `https://query1.finance.yahoo.com/v7/finance/spark?symbols=${stock_name}&range=1d&interval=5m&indicators=close&includeTimestamps=false&includePrePost=false&corsDomain=finance.yahoo.com&.tsrc=finance`
-  //   );
+app.get('/stocks/:stock_name', async ({ params: { stock_name = '' } }, res) => {
+  const result = await controller.getStockBySymbol(stock_name);
+  const apiResponse = new ApiResponse(result);
 
-  //   if (result) {
-  //     const { regularMarketPrice, regularMarketTime } = result[0].response[0].meta!;
-  //     res.send({
-  //       name: result[0].symbol,
-  //       lastPrice: regularMarketPrice,
-  //       pricedAt: new Date(Number(regularMarketTime.toString() + '000')).toISOString(),
-  //     });
-  //   }
-  // } catch (error: any) {
-  //   const errorObj = error.response.data.spark.error;
-  //   res.send(errorObj);
-  // }
+  res.status(apiResponse.getStatusCode()).send(apiResponse);
 });
 
 app.listen(3000);
