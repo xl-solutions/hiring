a2a49b3728d4bddd277c0477fb15f65e5905ff9e
AhhYeaah <vivi26743@hotmail.com>
2022-11-21 23:51:42 -0300

    Implemented singleton in ApiControler

--- src/controllers/ApiController.ts
+++ src/controllers/ApiController.ts
@@ -1,35 +1,69 @@
 import { ApiService } from '../services/ApiService';
-import { ApiError, ErrorTypes } from '../utils/types/ApiObjects/ApiError';
-import { ApiSuccess } from '../utils/types/ApiObjects/ApiSuccess';
-import { ApiResponse } from '../utils/types/ApiResponseFactory';
-import { GetStockBySymbol } from '../utils/types/EndpointsTypes';
+import { ControllerError, ErrorTypes } from '../utils/types/ControllerResponses/ControllerError';
+import { ControllerSuccess } from '../utils/types/ControllerResponses/ControllerSuccess';
+import { YahooApiErrorObject } from '../utils/types/YahooApi/YahooApiTypes';
 import { ParameterValidator } from '../utils/validations/ParameterValidator';
-import { ValidatationTypes } from '../utils/validations/validationTypes';
+import { ValidatationTypes } from '../utils/validations/Validators';
 
 export class ApiController {
-  constructor(private apiService = new ApiService()) {}
+  static instance: ApiController;
 
-  async getStockBySymbol(stock_name: string): Promise<ApiSuccess | ApiError[]> {
+  private constructor(private apiService = new ApiService()) {}
+
+  static getInstance(apiService?: ApiService): ApiController {
+    if (!ApiController.instance) {
+      ApiController.instance = new ApiController(apiService);
+    }
+    return ApiController.instance;
+  }
+
+  async getStockBySymbol(stock_name: string): Promise<ControllerSuccess | ControllerError[]> {
     const validationErrors = ParameterValidator.getValidationErrors([ValidatationTypes.STRING, { stock_name }]);
-    if (validationErrors) {
+    if (validationErrors !== undefined) {
       return validationErrors;
     }
 
-    const result = await this.apiService.getStockBySymbol(stock_name);
+    try {
+      const result = await this.apiService.getStockBySymbol(stock_name);
 
-    //only errors have code attr error
-    if ('code' in result) {
-      if (result.code === 'Not Found') {
-        return [new ApiError(ErrorTypes.NOT_FOUND, result.description)];
-      } else {
-        return [new ApiError(ErrorTypes.UNKNOWN, result.description)];
-      }
-    } else {
-      return new ApiSuccess({
+      return new ControllerSuccess({
         name: result.symbol,
         lastPrice: result.regularMarketPrice,
         pricedAt: new Date(Number(result.regularMarketTime.toString() + '000')).toISOString(),
       });
+    } catch (err: any) {
+      //typing
+      const error: YahooApiErrorObject = err;
+      if (error.code === 'Not Found') {
+        return [new ControllerError(ErrorTypes.NOT_FOUND, error.description)];
+      } else {
+        return [new ControllerError(ErrorTypes.UNKNOWN, error.description)];
+      }
+    }
+  }
+
+  async getStockBySymbol(stock_name: string): Promise<ControllerSuccess | ControllerError[]> {
+    const validationErrors = ParameterValidator.getValidationErrors([ValidatationTypes.STRING, { stock_name }]);
+    if (validationErrors !== undefined) {
+      return validationErrors;
+    }
+
+    try {
+      const result = await this.apiService.getStockBySymbol(stock_name);
+
+      return new ControllerSuccess({
+        name: result.symbol,
+        lastPrice: result.regularMarketPrice,
+        pricedAt: new Date(Number(result.regularMarketTime.toString() + '000')).toISOString(),
+      });
+    } catch (err: any) {
+      //typing
+      const error: YahooApiErrorObject = err;
+      if (error.code === 'Not Found') {
+        return [new ControllerError(ErrorTypes.NOT_FOUND, error.description)];
+      } else {
+        return [new ControllerError(ErrorTypes.UNKNOWN, error.description)];
+      }
     }
   }
 }
