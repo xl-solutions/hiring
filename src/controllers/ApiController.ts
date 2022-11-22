75e25bd04e3bd8154e78ad9a148c886d864253e6
AhhYeaah <vivi26743@hotmail.com>
2022-11-22 15:15:24 -0300

    adding one more endpoint and removed  useless typing in line 39

--- src/controllers/ApiController.ts
+++ src/controllers/ApiController.ts
@@ -1,9 +1,10 @@
-import { ApiService } from '../services/ApiService';
+import { YahooApiService } from '../services/YahooApiService';
 import { BaseError } from '../utils/errors/BaseError';
 import { NotFoundError } from '../utils/errors/NotFoundError';
 import { UnknownError } from '../utils/errors/UnknownError';
 // import { ControllerError, ErrorTypes } from '../utils/types/ControllerResponses/ControllerError';
 import { ControllerSuccess } from '../utils/types/ControllerResponses/ControllerSuccess';
+import { CompareStockBySymbols, GetStockBySymbol } from '../utils/types/EndpointsTypes';
 import { YahooApiErrorObject } from '../utils/types/YahooApi/YahooApiTypes';
 import { ParameterValidator } from '../utils/validations/ParameterValidator';
 import { ValidatationTypes } from '../utils/validations/Validators';
@@ -11,16 +12,16 @@ import { ValidatationTypes } from '../utils/validations/Validators';
 export class ApiController {
   static instance: ApiController;
 
-  private constructor(private apiService = new ApiService()) {}
+  private constructor(private apiService = new YahooApiService()) {}
 
-  static getInstance(apiService?: ApiService): ApiController {
+  static getInstance(apiService?: YahooApiService): ApiController {
     if (!ApiController.instance) {
       ApiController.instance = new ApiController(apiService);
     }
     return ApiController.instance;
   }
 
-  async getStockBySymbol(stock_name: string): Promise<ControllerSuccess | BaseError[]> {
+  async getStockBySymbol(stock_name: string): Promise<ControllerSuccess<GetStockBySymbol> | BaseError[]> {
     const validationErrors = ParameterValidator.getValidationErrors([ValidatationTypes.STRING, { stock_name }]);
     if (validationErrors !== undefined) {
       return validationErrors;
@@ -35,9 +36,7 @@ export class ApiController {
         pricedAt: new Date(Number(result.regularMarketTime.toString() + '000')).toISOString(),
       });
     } catch (err: any) {
-      //typing
-      const error: YahooApiErrorObject = err;
-      if (error.code === 'Not Found') {
+      if (err.code === 'Not Found') {
         return [new NotFoundError({ stock_name })];
       } else {
         return [new UnknownError()];
@@ -45,22 +44,27 @@ export class ApiController {
     }
   }
 
-  async compareStockBySymbol(stock_name: string, stocks: string[]) {
+  async CompareStockBySymbols(
+    stock_name: string,
+    stocks: string[]
+  ): Promise<ControllerSuccess<CompareStockBySymbols> | BaseError[]> {
     const stocksArray = [stock_name, ...stocks];
-    const results = await Promise.all(
-      stocksArray.map(async (stocks) => {
-        return this.getStockBySymbol(stocks);
-      })
-    );
-
+    const successArray: GetStockBySymbol[] = [];
     const errorArray: BaseError[] = [];
-    results.forEach((element) => {
-      if ('length' in element) {
-        errorArray.push(...element);
-      }
-    });
 
-    console.log(errorArray);
-    // return results;
+    for (const stock of stocksArray) {
+      const result = await this.getStockBySymbol(stock);
+
+      if (result instanceof ControllerSuccess) {
+        successArray.push(result.getResult());
+      } else {
+        errorArray.push(...result);
+      }
+    }
+
+    const hasAnyErrors = errorArray.length > 0;
+    if (hasAnyErrors) return errorArray;
+
+    return new ControllerSuccess({ lastPrices: successArray });
   }
 }
