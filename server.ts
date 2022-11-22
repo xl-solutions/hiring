eab36d2a4ee0574f9237fc724492df8e031a7e5b
AhhYeaah <vivi26743@hotmail.com>
2022-11-22 02:16:11 -0300

    fixed incorrect routing

--- server.ts
+++ server.ts
@@ -1,14 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-import { ApiController } from './src/controllers/ApiController';
-import { ApiResponseFactory } from './src/utils/types/ApiResponse/ApiResponseFactory';
 import v1StockRouter from './src/api/v1/routers/StockRouter';
+import { NotFoundError } from './src/utils/errors/NotFoundError';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use('v1/stocks', v1StockRouter);
+app.use('/v1/stocks', v1StockRouter);
 
 app.listen(3000);
