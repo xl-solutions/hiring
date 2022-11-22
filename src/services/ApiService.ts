3df534729e6a7f2fc111439f7b5a1c9d10e28373
AhhYeaah <vivi26743@hotmail.com>
2022-11-21 23:52:50 -0300

    throwing in service to make error handling easier in controller

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -4,13 +4,13 @@ import {
   YahooApiErrorResponse,
   YahooApiSuccessResponse,
   YahooApiUsefullInfo,
-} from '../utils/types/YahooApiTypes';
-import { desconstructErrorObject, desconstructSuccessObject } from '../utils/YahooObjectDesconstructor';
+} from '../utils/types/YahooApi/YahooApiTypes';
+import { desconstructErrorObject, desconstructSuccessObject } from '../utils/types/YahooApi/YahooObjectDesconstructor';
 export class ApiService {
   // Dependency injection for testing later
   constructor(private axios = importedAxios) {}
 
-  async getStockBySymbol(stockName: string): Promise<YahooApiUsefullInfo | YahooApiErrorObject> {
+  async getStockBySymbol(stockName: string): Promise<YahooApiUsefullInfo> {
     return this.axios
       .get<YahooApiSuccessResponse>(
         `https://query1.finance.yahoo.com/v7/finance/spark?symbols=${stockName}&range=1d&interval=5m&indicators=close&includeTimestamps=false&includePrePost=false&corsDomain=finance.yahoo.com&.tsrc=finance`
@@ -19,7 +19,7 @@ export class ApiService {
         return desconstructSuccessObject(data);
       })
       .catch((data: YahooApiErrorResponse) => {
-        return desconstructErrorObject(data);
+        throw desconstructErrorObject(data);
       });
   }
 }
