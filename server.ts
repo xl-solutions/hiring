be5264c1e238ed92fe79bc2cfe88e560a019f03c
AhhYeaah <vivi26743@hotmail.com>
2022-11-21 23:53:40 -0300

    api versioning and router

--- server.ts
+++ server.ts
@@ -1,21 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import { ApiController } from './src/controllers/ApiController';
-import { ApiResponse } from './src/utils/types/ApiResponseFactory';
+import { ApiResponseFactory } from './src/utils/types/ApiResponse/ApiResponseFactory';
+import v1StockRouter from './src/api/v1/routers/StockRouter';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-//Calling this here isents me of creating a controller instance in every api call
-const controller = new ApiController();
-
-app.get('/stocks/:stock_name', async ({ params: { stock_name = '' } }, res) => {
-  const result = await controller.getStockBySymbol(stock_name);
-  const apiResponse = new ApiResponse(result);
-
-  res.status(apiResponse.getStatusCode()).send(apiResponse);
-});
+app.use('v1/stocks', v1StockRouter);
 
 app.listen(3000);
