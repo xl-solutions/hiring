447511772a018fd22226feada97187165a1b2a59
AhhYeaah <vivi26743@hotmail.com>
2022-11-22 15:16:26 -0300

    adding new type for new endpoint in endpointRsponseTypes

--- src/utils/types/EndpointsTypes.ts
+++ src/utils/types/EndpointsTypes.ts
@@ -1,7 +1,11 @@
-export type EndpointsResponseTypes = GetStockBySymbol;
+export type EndpointsResponseTypes = GetStockBySymbol | CompareStockBySymbols;
 
 export type GetStockBySymbol = {
   name: string;
   lastPrice: number;
   pricedAt: string; // data e hora no formato ISO 8601, UTC
+};
+
+export type CompareStockBySymbols = {
+  lastPrices: GetStockBySymbol[];
 };
