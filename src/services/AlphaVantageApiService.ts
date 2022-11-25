f7f23adadc5de8970dfbc7177914d0670db8d20b
AhhYeaah <vivi26743@hotmail.com>
2022-11-25 09:41:32 -0300

    change to full

--- src/services/AlphaVantageApiService.ts
+++ src/services/AlphaVantageApiService.ts
@@ -6,10 +6,10 @@ export class AlphaVantageApiService {
   // Dependency injection for testing later
   constructor(private axios = importedAxios) {}
 
-  async getStockLimitedHistoryBySymbol(stock_name: string, from: Date, to: Date) {
+  async getStockLimitedHistoryBySymbol(stock_name: string) {
     return this.axios
       .get(
-        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stock_name}&outputsize=compact&apikey=${process.env.API_KEY}`,
+        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stock_name}&outputsize=full&apikey=${process.env.API_KEY}`,
         { responseType: 'stream' }
       )
       .then(({ data }) => {
