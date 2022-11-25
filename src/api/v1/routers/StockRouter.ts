294eea775884482ba462cd0e7617153b0175ad86
AhhYeaah <vivi26743@hotmail.com>
2022-11-25 09:40:49 -0300

    added last route

--- src/api/v1/routers/StockRouter.ts
+++ src/api/v1/routers/StockRouter.ts
@@ -33,4 +33,14 @@ stockRouter.get('/:stock_name/history', async ({ params: { stock_name = '' }, qu
   res.status(apiResponse.getStatusCode()).send(apiResponse);
 });
 
+stockRouter.get(
+  '/:stock_name/gains',
+  async ({ params: { stock_name = '' }, query: { purchasedAmount = '', purchasedAt = '' } }, res) => {
+    const result = await controller.projectGains(stock_name, purchasedAmount.toString(), purchasedAt.toString());
+    const apiResponse = ApiResponseFactory.createResponseInstance(result);
+
+    res.status(apiResponse.getStatusCode()).send(apiResponse);
+  }
+);
+
 export default stockRouter;
