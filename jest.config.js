77f771eec223bb057d02d2e430715036eb3be452
AhhYeaah <vivi26743@hotmail.com>
2022-11-25 10:51:20 -0300

    added jest and ts-jest

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: [ "**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)" ]
+  };
