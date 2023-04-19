bb7cbbd144378bafc96d581def3fac580fed6f4c
Rrissoli <raphels123@gmail.com>
2023-04-19 16:17:17 -0300

    path modal details created

--- src/components/patterns/Footer/index.jsx
+++ src/components/patterns/Footer/index.jsx
@@ -1,8 +1,13 @@
-import { theme } from '../../../theme/theme';
-import { Box, Text } from '../../../theme/components';
-
+import styles from './styles.module.css'
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  const projectName = "Projeto Raphael Rissoli";
+
   return (
-    
-  )
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <p>{projectName} - {currentYear}</p>
+      </div>
+    </footer>
+  );
 }
